141cb774e82281698750ff65000c6f4f334b99ec
KOgamerX <98529891+10Knockout@users.noreply.github.com>
2026-08-31 17:56:02 +0530

    feat(protocol): export product shell schemas

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,6 +1,9 @@
 export * from "./brands.js";
 export * from "./schemas/handshake.js";
 export * from "./schemas/auth.js";
+export * from "./schemas/profile.js";
+export * from "./schemas/settings.js";
+export * from "./schemas/leaderboard.js";
 
 export * from "./binary/writer.js";
 export * from "./binary/reader.js";
